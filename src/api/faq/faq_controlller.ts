54d4fbafbccdd8cef2ad3491af8e8473817c705c
sandeep0009 <500sandeepks@gmail.com>
2025-02-01 00:08:06 +0530

    feat:added redis

--- src/api/faq/faq_controlller.ts
+++ src/api/faq/faq_controlller.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
 import { faq } from "../../schema/faq_scehma";
 import { faqMessge } from "../../constant/constant";
+import { redis } from "../../helper/redis";
 export const getFaq = async (req: Request, res: Response): Promise<any> => {
     try {
         const { lang } = req.query;
         const requestedLanguage = lang ? (lang as string) : 'en';
         const allFaq = await faq.find();
 
+        const cacheKey = `faq:${requestedLanguage}`;
+        const cachedData = await redis.get(cacheKey);
+
+
+
+        if (cachedData) {
+            console.log("Serving from Redis Cache");
+            return res.status(200).json({ message: faqMessge.fetched, faqWithTranslation: JSON.parse(cachedData) });
+        }
         let faqWithTranslation = allFaq
             .map((item) => {
                 const translation = item.translation.get(requestedLanguage);
@@ -19,6 +29,7 @@ export const getFaq = async (req: Request, res: Response): Promise<any> => {
                 return null;
             })
             .filter((item) => item !== null);
+            await redis.set(cacheKey, JSON.stringify(faqWithTranslation), "EX", 3600);
         res.status(200).json({ message: faqMessge.fetched, faqWithTranslation });
     } catch (error) {
         console.error("Error fetching FAQs:", error);
